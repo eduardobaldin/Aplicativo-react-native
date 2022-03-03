2684efdb790b621d4b30c12c5819443ac1561521
Eduardo Heimerdinger <edu.heimerdinger@gmail.com>
2022-03-02 22:50:58 -0300

    3 commit

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {View,Text,Button,Alert,StyleSheet} from 'react-native';
+import {View,Text,Button,Alert,StyleSheet, StatusBar, Image} from 'react-native';
+
 
 import Navbar from './componentes/Navbar';
 
 export default function App(){
   return(
     <View>
+      <StatusBar style="ligth" backgroundColor='blue' />
       <Navbar />
-      <Button style={estilo.btn}
-        title='Camera'
-        onPress={()=>Alert.alert('Foto','foto tirada')} 
-        
-      />
+      <View style={estilo.align}>
+        <Image style={estilo.icon} source={ {uri: 'https://cdn-icons.flaticon.com/png/512/3288/premium/3288788.png?token=exp=1646272176~hmac=610d9cd3340d9911378f2ff3ee5d80da'}} />
+      </View>
     </View>
   );
 };
@@ -22,4 +22,15 @@ const estilo=StyleSheet.create({
     backgroundColor:'ff4081',
     width:10    
   },
+  icon:{
+    width:40,
+    height:40,
+    backgroundColor:'#ff4081',
+    borderRadius:15,
+    margin:1,
+  },
+  align:{
+    alignItems:'flex-end',
+    
+  }  
 })
