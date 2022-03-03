19ab0af3633e37dced41132b4ca1fa2cac5a73e7
Eduardo Heimerdinger <edu.heimerdinger@gmail.com>
2022-03-02 21:42:45 -0300

    2 commit

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App(){
     <View>
       <Navbar />
       <Button style={estilo.btn}
-        title='oi'
+        title='Camera'
         onPress={()=>Alert.alert('Foto','foto tirada')} 
         
       />
